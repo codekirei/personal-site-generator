3175ed5a0b1f6eb9bbec9538fd14e09701a0b9b0
Jacob Blakely <jacob@codekirei.com>
2015-11-17 21:26:48 -0800

    gulp scripts -> gulp 3

--- gulpfile.js/tasks/pipeline/scripts.js
+++ gulpfile.js/tasks/pipeline/scripts.js
@@ -5,7 +5,6 @@
 //----------------------------------------------------------
 // npm
 const gulp = require('gulp')
-const P = require('bluebird')
 const g = require('gulp-load-plugins')()
 
 // local
@@ -16,18 +15,15 @@ const loc = require('conf/locations')
 // logic
 //----------------------------------------------------------
 function scripts() {
-  return new P((res, rej) => {
-    gulp.src(loc.src.scripts)
-      .pipe(g.if(flags.watching, g.plumber()))
-      .pipe(g.concat('main.js'))
-      .pipe(g.uglify())
-      .pipe(gulp.dest(loc.dist.code))
-    res()
-  })
+  return gulp.src(loc.src.scripts)
+    .pipe(g.if(flags.watching, g.plumber()))
+    .pipe(g.concat('main.js'))
+    .pipe(g.uglify())
+    .pipe(gulp.dest(loc.dist.code))
 }
 
 //----------------------------------------------------------
 // exports
 //----------------------------------------------------------
 module.exports = scripts
-// gulp.task(scripts)
+gulp.task('scripts', scripts)
