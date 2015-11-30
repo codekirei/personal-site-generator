5b3e95efc4e3b64a8744d2127192107b2e02e50a
Jacob Blakely <jacob@codekirei.com>
2015-11-30 12:07:21 -0800

    swap out getEl for get-element

--- source/scripts/fixHeight.js
+++ source/scripts/fixHeight.js
@@ -1,17 +1,17 @@
-import {getTag, getClass} from './utils/getEl'
+import el from 'get-element'
 
 function fixHeight() {
   // hack to target mobile
   if (typeof window.orientation !== 'undefined') {
-    const html = getTag('html')[0]
-    const body = getTag('body')[0]
-    const banner = getClass('index-banner')[0]
+    const html = el.withTag('html')[0]
+    const body = el.withTag('body')[0]
+    const banner = el.withClass('index-banner')[0]
     const height = window.innerHeight
     const els = [html, body, banner]
     els.map(el => {
       el.style.height = `${height}px`
     })
-    const arrow = getClass('scroll-down')[0]
+    const arrow = el.withClass('scroll-down')[0]
     const arrowHeight = getComputedStyle(arrow)['height']
       .replace('px', '')
     const top = height - parseInt(arrowHeight)
--- source/scripts/utils/getEl.js
+++
@@ -1,9 +0,0 @@
-function getTag(tagName) {
-  return Array.prototype.slice.call(document.getElementsByTagName(tagName))
-}
-
-function getClass(className) {
-  return Array.prototype.slice.call(document.getElementsByClassName(className))
-}
-
-export {getTag, getClass}
