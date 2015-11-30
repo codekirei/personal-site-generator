80c0b113d37dff6833453d05d21aa608c86d47cf
Jacob Blakely <jacob@codekirei.com>
2015-11-30 13:06:47 -0800

    add scroll-down js functionality

---
+++ source/scripts/scroll-down.js
@@ -0,0 +1,12 @@
+import smoothScroll from 'smooth-scroll'
+
+export default smoothScroll.init(
+  { selector: '[data-scroll]'
+  // , selectorHeader:
+  , speed: 500
+  , easing: 'easeInOutCubic'
+  , updateURL: false
+  , offset: 0
+  // , callback:
+  }
+)
