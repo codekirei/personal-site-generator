9f67a6ce9dda8bfabd8b8c65459bdec74de05435
Jacob Blakely <jacob@codekirei.com>
2015-12-04 18:53:46 -0800

    add some clarity comments to menu.js

--- source/scripts/main/menu.js
+++ source/scripts/main/menu.js
@@ -58,7 +58,7 @@ function menuEvents() {
   //----------------------------------------------------------
   menu.style.opacity = opacity
 
-  // hacky test for mobile
+  // hacky test for mobile because they have clear scrollbars (feelsbadman)
   //----------------------------------------------------------
   const mobile = typeof window.orientation !== 'undefined'
 
@@ -73,22 +73,32 @@ function menuEvents() {
   const a = (fn, cb) => animate(menu, fn, 322, ease.easeIn, cb)
 
   function preOpen() {
+    // add --state styles
     elcl.ensure(menu, 'visible')
     elcl.ensure(body, 'no-scroll')
+
+    // account for scrollbar disappearing
     if (!mobile) {
       elst.add([body, banner], {paddingRight: `${scrollW}px`})
       elst.add(banner, {width: `${windowW}px`})
     }
+
+    // toggle
     shouldPreOpen = false
   }
 
   function postClose() {
+    // remove --state styles
     elcl.toggle(menu, 'visible')
     elcl.toggle(body, 'no-scroll')
+
+    // account for scrollbar reappearing
     if (!mobile) {
       elst.del(body, 'padding-right')
       elst.del(banner, ['width', 'padding-right'])
     }
+
+    // toggle
     shouldPreOpen = true
   }
 
@@ -100,12 +110,23 @@ function menuEvents() {
   const close = () => animations.out = a(fadeOut, postClose)
 
   function toggleMenu(e) {
+    // don't follow href
     e.preventDefault()
+
+    // scroll to top if not there already
     if (window.pageYOffset !== 0) window.scroll(0,0)
-    directionToggle = !directionToggle
+
+    // stop all running animations
     Object.keys(animations).map(anim => animations[anim].stop())
+
+    // store current menu opacity
     opacity = parseFloat(menu.style.opacity)
+
+    // toggles
+    directionToggle = !directionToggle
     elcl.toggle(menuButton, 'toggled')
+
+    // open or close menu
     return directionToggle ? close() : open()
   }
 
