75dbd8fc0935528548128baece4887291075f97d
Jacob Blakely <jacob@codekirei.com>
2016-01-05 13:15:33 -0800

    remove dead code

--- source/markup/templates/modules/topbar.js
+++ source/markup/templates/modules/topbar.js
@@ -27,28 +27,6 @@ const topbarTitle = data =>
     , data.title
   )
 
-// function topbar(data) {
-//   const header =
-//     m('header'
-//       , { class: 'topbar'
-//         , role: 'heading'
-//         }
-//       , [ content ])
-
-//   const content =
-//     m('div'
-//       , { class: 'topbar__content' }
-//       , [ title(data) ])
-
-//   const title = data =>
-//     m('h1'
-//       , { class: 'topbar__title' }
-//       , data.title
-//     )
-
-//   return header
-// }
-
 //----------------------------------------------------------
 // exports
 //----------------------------------------------------------
